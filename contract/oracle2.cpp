fbee4b749e37d7c169d1c15034c3138537e9a5ca
systemzax <gbabint@msn.com>
2018-10-13 16:54:11 -0400

    misc

--- contract/oracle2.cpp
+++ contract/oracle2.cpp
@@ -170,8 +170,7 @@ class DelphiOracle : public eosio::contract {
           s.timestamp = current_time();
         });
 
-        usdtable va_sort(get_self(), get_self());
-        auto value_sorted = va_sort.get_index();
+        auto value_sorted = usdstore.get_index<N(value)>();
 
         value_sorted.begin();
         value_sorted+=6;
