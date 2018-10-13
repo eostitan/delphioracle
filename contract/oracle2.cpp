594cd116c5da60a7b0ea4f7912e7188d2280eb47
systemzax <gbabint@msn.com>
2018-10-13 12:55:32 -0400

    Misc

--- contract/oracle2.cpp
+++ contract/oracle2.cpp
@@ -208,7 +208,7 @@ class DelphiOracle : public eosio::contract {
 
   //Update oracles list
   [[eosio::action]]
-  void setoracles(const std::vector<account_name>& oracles_list) {
+  void setoracles(const account_name[] oracleslist) {
     
     require_auth(titan_account);
 
