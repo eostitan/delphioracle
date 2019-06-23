642270ff1db1c9279200086a0857169512e795f3
systemzax <gbabint@msn.com>
2019-06-23 09:51:29 -0400

    Cleaned up updater.js example

--- scripts/updater.js
+++ scripts/updater.js
@@ -1,8 +1,10 @@
 const Eos = require('eosjs');
 const dotenv = require('dotenv');
 const axios = require('axios');
+const request = require('request');
 
-const url = "https://min-api.cryptocompare.com/data/price?fsym=EOS&tsyms=USD&e=Kraken";
+const usdUrl = "https://min-api.cryptocompare.com/data/price?fsym=EOS&tsyms=USD";
+const btcUrl = "https://min-api.cryptocompare.com/data/price?fsym=EOS&tsyms=BTC";
 
 dotenv.load();
 
@@ -23,53 +25,46 @@ const eos = Eos({
 
 function write(){
 
-	axios.get(`${url}`)
-		.then(results=>{
 
-			if (results.data && results.data.USD){
+	request.get(usdUrl, function (err, res, usdRes){
+		request.get(btcUrl, function (err, res, btcRes){
+			
+			console.log("USD:", JSON.parse(usdRes).USD);
+			console.log("BTC:", JSON.parse(btcRes).BTC);
 
-				console.log(" results.data.USD",  results.data.USD);
+			var quotes = [{"value": parseInt(Math.round(JSON.parse(btcRes).BTC * 100000000)), pair:"eosbtc"}, 
+										{"value": parseInt(Math.round(JSON.parse(usdRes).USD * 10000)), pair:"eosusd"}];
 
-				eos.contract(oracleContract)
-					.then((contract) => {
-						contract.write({
-								owner: owner,
-								value: parseInt(Math.round(results.data.USD * 10000)),
-								symbol: "eosusd"
-							},
-							{
-								scope: oracleContract,
-								authorization: [`${owner}@${process.env.ORACLE_PERMISSION || 'active'}`] 
-							})
-							.then(results=>{
-								console.log("results:", results);
-								setTimeout(write, interval);
-							})
-							.catch(error=>{
-								console.log("error:", error);
-								setTimeout(write, interval);
-							});
+			console.log("quotes:", quotes);
 
-					})
-					.catch(error=>{
-						console.log("error:", error);
-						setTimeout(write, interval);
-					});
+			eos.contract(oracleContract)
+				.then((contract) => {
+					contract.write({
+							owner: owner,
+							quotes: quotes
+						},
+						{
+							scope: oracleContract,
+							authorization: [`${owner}@${process.env.ORACLE_PERMISSION || 'active'}`] 
+						})
+						.then(results=>{
+							console.log("results:", results);
+						})
+						.catch(error=>{
+							console.log("error:", error);
+						});
 
-			}
-			else setTimeout(write, interval);
+				})
+				.catch(error=>{
+					console.log("error:", error);
+				});
 
-		})
-		.catch(error=>{
-			console.log("error:", error);
-			setTimeout(write, interval);
 		});
+
+	});
 
 }
 
 
 write();
-
-//setInterval(write, 60000);
-
 
